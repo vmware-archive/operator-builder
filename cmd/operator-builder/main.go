5102e241c802b0f2199d7e956d064e503e75fc83
Jeff Davis <jeffda@vmware.com>
2021-09-08 11:23:13 -0400

    refactor: move cli to pkg directory
    Signed-off-by: Jeff Davis <jeffda@vmware.com>

--- cmd/operator-builder/main.go
+++ cmd/operator-builder/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	log "github.com/sirupsen/logrus"
 
-	"github.com/vmware-tanzu-labs/operator-builder/internal/cli"
+	"github.com/vmware-tanzu-labs/operator-builder/pkg/cli"
 )
 
 func main() {
