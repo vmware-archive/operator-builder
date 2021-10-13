935f120496f711c0767959ecf425f42e47d9f783
Dustin Scott <sdustin@vmware.com>
2021-10-13 13:29:51 -0500

    test: add unit tests for companion cli
    Signed-off-by: Dustin Scott <sdustin@vmware.com>

---
+++ internal/workload/v1/collection_test.go
@@ -0,0 +1,195 @@
+// Copyright 2021 VMware, Inc.
+// SPDX-License-Identifier: MIT
+
+package v1
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_CollectionSetNames(t *testing.T) {
+	sharedNameInput := WorkloadShared{
+		Name: "shared-name",
+		Kind: "WorkloadCollection",
+	}
+
+	sharedNameExpected := WorkloadShared{
+		Name:        "shared-name",
+		PackageName: "sharedname",
+		Kind:        "WorkloadCollection",
+	}
+
+	for _, tt := range []struct {
+		name     string
+		input    *WorkloadCollection
+		expected *WorkloadCollection
+	}{
+		{
+			name: "workload collection missing root command",
+			input: &WorkloadCollection{
+				WorkloadShared: sharedNameInput,
+			},
+			expected: &WorkloadCollection{
+				WorkloadShared: sharedNameExpected,
+				Spec: WorkloadCollectionSpec{
+					CompanionCliRootcmd: CliCommand{},
+				},
+			},
+		},
+		{
+			name: "workload collection with root command missing description",
+			input: &WorkloadCollection{
+				WorkloadShared: sharedNameInput,
+				Spec: WorkloadCollectionSpec{
+					API: APISpec{
+						Kind: "WorkloadCollectionTest",
+					},
+					CompanionCliRootcmd: CliCommand{
+						Name: "hasrootcommand",
+					},
+				},
+			},
+			expected: &WorkloadCollection{
+				WorkloadShared: sharedNameExpected,
+				Spec: WorkloadCollectionSpec{
+					API: APISpec{
+						Kind: "WorkloadCollectionTest",
+					},
+					CompanionCliRootcmd: CliCommand{
+						Name:        "hasrootcommand",
+						Description: "Manage workloadcollectiontest collection and components",
+						VarName:     "Hasrootcommand",
+						FileName:    "hasrootcommand",
+					},
+					CompanionCliSubcmd: CliCommand{
+						Name:        "collection",
+						Description: "Manage workloadcollectiontest workload",
+						VarName:     "Collection",
+						FileName:    "collection",
+					},
+				},
+			},
+		},
+		{
+			name: "workload collection with root command",
+			input: &WorkloadCollection{
+				WorkloadShared: sharedNameInput,
+				Spec: WorkloadCollectionSpec{
+					API: APISpec{
+						Kind: "WorkloadCollectionTest",
+					},
+					CompanionCliRootcmd: CliCommand{
+						Name:        "hasrootcommand",
+						Description: "Manage workloadcollectiontest collection and components custom",
+					},
+				},
+			},
+			expected: &WorkloadCollection{
+				WorkloadShared: sharedNameExpected,
+				Spec: WorkloadCollectionSpec{
+					API: APISpec{
+						Kind: "WorkloadCollectionTest",
+					},
+					CompanionCliRootcmd: CliCommand{
+						Name:        "hasrootcommand",
+						Description: "Manage workloadcollectiontest collection and components custom",
+						VarName:     "Hasrootcommand",
+						FileName:    "hasrootcommand",
+					},
+					CompanionCliSubcmd: CliCommand{
+						Name:        "collection",
+						Description: "Manage workloadcollectiontest workload",
+						VarName:     "Collection",
+						FileName:    "collection",
+					},
+				},
+			},
+		},
+		{
+			name: "workload collection with full subcommand",
+			input: &WorkloadCollection{
+				WorkloadShared: sharedNameInput,
+				Spec: WorkloadCollectionSpec{
+					API: APISpec{
+						Kind: "WorkloadCollectionTest",
+					},
+					CompanionCliRootcmd: CliCommand{
+						Name: "hasrootcommand",
+					},
+					CompanionCliSubcmd: CliCommand{
+						Name:        "collection",
+						Description: "Manage workloadcollectiontest workload custom",
+						VarName:     "Collection",
+						FileName:    "collection",
+					},
+				},
+			},
+			expected: &WorkloadCollection{
+				WorkloadShared: sharedNameExpected,
+				Spec: WorkloadCollectionSpec{
+					API: APISpec{
+						Kind: "WorkloadCollectionTest",
+					},
+					CompanionCliRootcmd: CliCommand{
+						Name:        "hasrootcommand",
+						Description: "Manage workloadcollectiontest collection and components",
+						VarName:     "Hasrootcommand",
+						FileName:    "hasrootcommand",
+					},
+					CompanionCliSubcmd: CliCommand{
+						Name:        "collection",
+						Description: "Manage workloadcollectiontest workload custom",
+						VarName:     "Collection",
+						FileName:    "collection",
+					},
+				},
+			},
+		},
+		{
+			name: "workload collection with full subcommand missing description",
+			input: &WorkloadCollection{
+				WorkloadShared: sharedNameInput,
+				Spec: WorkloadCollectionSpec{
+					API: APISpec{
+						Kind: "WorkloadCollectionTest",
+					},
+					CompanionCliRootcmd: CliCommand{
+						Name: "hasrootcommand",
+					},
+					CompanionCliSubcmd: CliCommand{
+						Name:     "collection",
+						VarName:  "Collection",
+						FileName: "collection",
+					},
+				},
+			},
+			expected: &WorkloadCollection{
+				WorkloadShared: sharedNameExpected,
+				Spec: WorkloadCollectionSpec{
+					API: APISpec{
+						Kind: "WorkloadCollectionTest",
+					},
+					CompanionCliRootcmd: CliCommand{
+						Name:        "hasrootcommand",
+						Description: "Manage workloadcollectiontest collection and components",
+						VarName:     "Hasrootcommand",
+						FileName:    "hasrootcommand",
+					},
+					CompanionCliSubcmd: CliCommand{
+						Name:        "collection",
+						Description: "Manage workloadcollectiontest workload",
+						VarName:     "Collection",
+						FileName:    "collection",
+					},
+				},
+			},
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.input.SetNames()
+			assert.Equal(t, tt.expected, tt.input)
+		})
+	}
+}
