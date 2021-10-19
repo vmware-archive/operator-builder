33563b9ded332adad2f39fd7a088bcae31d9b032
Jeff Davis <jeffda@vmware.com>
2021-10-19 15:56:34 -0400

    feat: add funcs to build map from dot notation
    Signed-off-by: Jeff Davis <jeffda@vmware.com>

--- internal/workload/v1/spec_field.go
+++ internal/workload/v1/spec_field.go
@@ -4,8 +4,12 @@
 package v1
 
 import (
+	"errors"
 	"fmt"
+	"strings"
 )
+
+var ErrOverwriteExistingValue = errors.New("an attempt to overwrite existing value was made")
 
 // APISpecField represents a single field in a custom API type.
 type APISpecField struct {
@@ -30,4 +34,43 @@ func (api *APISpecField) setSampleAndDefault(name string, value interface{}) {
 		api.DefaultVal = fmt.Sprintf("%v", value)
 		api.SampleField = fmt.Sprintf("%s: %v", name, value)
 	}
+}
+
+func (api *APISpecField) BuildMap(output map[string]interface{}) error {
+	obj := output
+	parts := strings.Split(api.Path, ".")
+	last := parts[len(parts)-1]
+
+	for _, part := range parts[:len(parts)-1] {
+		if _, ok := obj[part].(map[string]interface{}); !ok {
+			if obj[part] != nil {
+				return fmt.Errorf("%w at %s", ErrOverwriteExistingValue, api.Path)
+			}
+			obj[part] = make(map[string]interface{})
+		}
+
+		//nolint: forcetypeassert // type checking occurs above
+		obj = obj[part].(map[string]interface{})
+	}
+
+	if obj[last] != nil {
+		return fmt.Errorf("%w at %s", ErrOverwriteExistingValue, api.Path)
+	}
+
+	obj[last] = api.DefaultVal
+
+	return nil
+}
+
+func (apis *APISpecFields) BuildMaps(output map[string]interface{}) error {
+	a := *apis
+
+	for i := range a {
+		err := a[i].BuildMap(output)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
